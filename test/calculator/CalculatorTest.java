7b9434d7e80a7c4e752920ae992e7afec2b3e509
Hoseon Yoon <gogohigh@naver.com>
2017-05-04 00:44:24 +0900

    refactoring

---
+++ test/calculator/CalculatorTest.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	private Calculator cal;
+	
+	@Before
+	public void setup() {
+		cal = new Calculator();
+		System.out.println("setup!");
+	}
+
+	@Test
+	public void add() {
+		assertEquals(3, cal.add(2, 1));
+		System.out.println("add!");
+	}
+	
+	@Test
+	public void divide() {
+		assertEquals(3, cal.divide(9, 3));
+		System.out.println("divide!");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown!");
+	}
+}
